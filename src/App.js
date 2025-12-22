6dfebb36db428c5b51b30ac8ab54d9b90eeec5a7
Vansh Patel <patel-vansh1607@gmail.com>
2025-12-22 22:21:54 +0300

    ADDED

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Mainbar from './components/Mainbar';
 import Navbar from './components/Navbar';
 import NewsTicker from './components/NewsTicker';
-
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,33 @@ function App() {
       <Mainbar />
       <div className='img'>
         <img className = "image1"src="https://res.cloudinary.com/dxgkcyfrl/image/upload/v1766427632/Untitled-1-01_bl5svr.jpg" alt="Main_Banner" />
-      </div>
+      </div> 
+      <p className='ttt'>Please Select the day you want to view</p> 
+      <div className="all">
+      <a
+        href="/live-day-1-hanuman-murti-inaugration"
+        className="btn-text"
+      >
+        Day 1 | Friday, 23<sup>rd</sup> January 2026
+      </a>
+      <br />
+
+      <a
+        href="/live-day-2-hanuman-murti-inaugration"
+        className="btn-text"
+      >
+        Day 2 | Friday, 24<sup>th</sup> January 2026
+      </a>
+      <br />
+
+      <a
+        href="/live-day-3-hanuman-murti-inaugration"
+        className="btn-text"
+      >
+        Day 3 | Friday, 25<sup>th</sup> January 2026
+      </a>
+    </div>
+
     </div>
   );
 }
